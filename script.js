500a6fa8e80e9cc8172966947bcef8829a87c7f8
mhmoedmhmed994-byte <mhmoedmhmed994@gmail.com>
2026-01-27 07:47:17 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.getElementById("loginBtn").onclick = () => {
+  // بعد ما يضغط Login يروح لل Dashboard
+  window.location.href = "dashboard.html";
+};
 
+document.getElementById("googleBtn").onclick = () => {
+  alert("Google login clicked");
+};
+
+document.getElementById("guestBtn").onclick = () => {
+  window.location.href = "dashboard.html";
+};
