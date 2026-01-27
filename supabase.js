2e7f4955de3fe736ae02108dc1e1ab9031fb7d99
mhmoedmhmed994-byte <mhmoedmhmed994@gmail.com>
2026-01-27 10:09:06 -0800

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const supabaseUrl = "YOUR_SUPABASE_URL";
-const supabaseKey = "YOUR_SUPABASE_ANON_KEY";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
