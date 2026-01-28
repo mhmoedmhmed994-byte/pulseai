cd3be1d8c9f6affeeededcb4598ec6dd4d796537
mhmoedmhmed994-byte <mhmoedmhmed994@gmail.com>
2026-01-28 20:55:36 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = "https://kiukgdrkctbtknimkpds.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
-
-const supabase = supabaseCreateClient(SUPABASE_URL, SUPABASE_ANON_KEY);
